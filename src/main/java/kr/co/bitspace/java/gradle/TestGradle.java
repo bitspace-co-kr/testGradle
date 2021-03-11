ea71a217c264493c3fd6f660456f1a6fdf92a7a0
성훈 김 <mymessage.scm@gmail.com>
2021-03-11 23:46:14 +0900

    initial

---
+++ src/main/java/kr/co/bitspace/java/gradle/TestGradle.java
@@ -0,0 +1,11 @@
+package kr.co.bitspace.java.gradle;
+
+public class TestGradle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gradle Test");
+
+	}
+
+}
